4456e9d014462e1f982552e01c5ca20425dfb896
Yeldos <eldos.amire@gmail.com>
2024-06-09 15:50:56 +0500

    Configured the LoginViewModel and SignupViewModel to interact with the repository

--- app/src/main/java/com/example/authenticate/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/authenticate/viewmodel/LoginViewModel.kt
@@ -5,20 +5,22 @@ import androidx.lifecycle.viewModelScope
 import com.example.authenticate.data.model.User
 import com.example.authenticate.data.repository.AuthRepository
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class LoginViewModel(
     private val authRepository: AuthRepository
 ) : ViewModel() {
 
-    private val _loginState = MutableSharedFlow<Result<User>>()
-    val loginState: SharedFlow<Result<User>> get() = _loginState
+    private val _loginState = MutableStateFlow<Result<User>?>(null)
+    val loginState: StateFlow<Result<User>?> get() = _loginState
 
     fun login(email: String, password: String) {
         viewModelScope.launch {
             authRepository.login(email, password).collect {
-                _loginState.emit(it)
+                _loginState.value = it
             }
         }
     }
--- app/src/main/java/com/example/authenticate/viewmodel/SignupViewModel.kt
+++ app/src/main/java/com/example/authenticate/viewmodel/SignupViewModel.kt
@@ -5,20 +5,22 @@ import androidx.lifecycle.viewModelScope
 import com.example.authenticate.data.model.User
 import com.example.authenticate.data.repository.AuthRepository
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class SignupViewModel(
     private val authRepository: AuthRepository
 ) : ViewModel() {
 
-    private val _signupState = MutableSharedFlow<Result<User>>()
-    val signupState: SharedFlow<Result<User>> get() = _signupState
+    private val _signupState = MutableStateFlow<Result<User>?>(null)
+    val signupState: StateFlow<Result<User>?> get() = _signupState
 
     fun signup(name: String, email: String, password: String) {
         viewModelScope.launch {
             authRepository.signup(name, email, password).collect {
-                _signupState.emit(it)
+                _signupState.value = it
             }
         }
     }
