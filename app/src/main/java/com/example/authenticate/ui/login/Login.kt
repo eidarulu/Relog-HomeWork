f8c48ba660190d517a51241196bcc3a15b6e8782
Yeldos <eldos.amire@gmail.com>
2024-06-15 16:48:37 +0500

    Enabled error messages pop up notifications

--- app/src/main/java/com/example/authenticate/ui/login/Login.kt
+++ app/src/main/java/com/example/authenticate/ui/login/Login.kt
@@ -1,5 +1,6 @@
 package com.example.authenticate.ui.login
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -21,6 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -28,9 +30,9 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.authenticate.R
+import com.example.authenticate.ui.component.HaveAccountRow
 import com.example.authenticate.ui.component.items.AuthButton
 import com.example.authenticate.ui.component.items.AuthTextField
-import com.example.authenticate.ui.component.HaveAccountRow
 import org.koin.androidx.compose.getViewModel
 
 @Composable
@@ -125,6 +127,7 @@ fun LoginScreen(navController: NavController) {
                     },
                     text = stringResource(id = R.string.login),
                 )
+                NotificationMessage(loginViewModel)
                 Spacer(modifier = Modifier.height(8.dp))
                 if (loginViewModel.signedIn.value) {
                     navController.navigate("main")
@@ -140,5 +143,14 @@ fun LoginScreen(navController: NavController) {
                 )
             }
         }
+    }
+}
+
+@Composable
+fun NotificationMessage(lvm: LoginViewModel) {
+    val notifState = lvm.popupNotification.value
+    val notifMessage = notifState?.getContentOrNull()
+    if (notifMessage != null) {
+        Toast.makeText(LocalContext.current, notifMessage, Toast.LENGTH_SHORT).show()
     }
 }
